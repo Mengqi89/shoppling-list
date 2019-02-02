adc2ae698b51b8516f41ea50de65ca1f56002e44
Mengqi Wang <wmq516@gmail.com>
2019-02-02 11:17:06 -0700

    added toggleClass

--- index.js
+++ index.js
@@ -3,17 +3,38 @@ function getInputItem () {
     function() {
        event.preventDefault();
 
-
       let input = $('.js-shopping-list-entry').val();
 
       console.log(input);
+      $('.shopping-list').append(`<li>
+      <span class="shopping-item">${input}</span>
+      <div class="shopping-item-controls">
+        <button class="shopping-item-toggle">
+          <span class="button-label">check</span>
+        </button>
+        <button class="shopping-item-delete">
+          <span class="button-label">delete</span>
+        </button>
+      </div>
+    </li>`)
+      
     });
 };
 
+function toggleCheck() {
+  $(".shopping-list").on('click', '.shopping-item-toggle', function(event) {
+    console.log('toggle button');
+   $('.shopping-item').toggleClass('shopping-item__checked');
+   console.log($(this));
+   
+  }
+  )
+}
 
 
 function startApp() {
   getInputItem();
+  toggleCheck();
 }
 
 $(startApp());
